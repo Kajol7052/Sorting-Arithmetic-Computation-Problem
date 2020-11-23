74b13be77af508cd17bfd2fd17c27485eb1ea042
“Kajol7052” <kajol7052@gmail.com>
2020-11-23 09:14:21 +0530

    Added : Store all Results in Dictionary

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -2,6 +2,9 @@
 echo "Welcome to Arithmetic Computing and Sorting"
 echo ""
 
+#Syntax to declare Dictionary
+declare -A Sort
+
 #UC-1 : To take inputs from the user a, b and c
 read -p "Enter first input :" a
 read -p "Enter second input:" b
@@ -24,3 +27,6 @@ echo "Result3 : $res3"
 res4=$((a%b+c))
 echo "Result4 : $res4"
 echo ""         #to print a new line
+
+#UC-6 : To store the results in dictionary for all computations
+Sort=( [res1]=$res1 [res2]=$res2 [res3]=$res3 [res4]=$res4 )
